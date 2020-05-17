a7c3c6d31fb339cc97174baa8f910f00579e7b17
ChawkiOS <chawkios2@gmail.com>
2020-05-17 06:13:18 +0200

    init mapbox with active theme mode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	private mapbox: mapboxgl.Map;
 
-  constructor() {}
+	constructor() {
+		mapboxgl.accessToken = environment.mapbox.accessToken;
+	}
 
+	ngOnInit() {
+		const mediaDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+		mediaDarkMode.addListener(this.switchMode);
+
+		this.initMap(mediaDarkMode.matches);
+	}
+
+	private initMap(isDarkModeEnebled: boolean) {
+		this.mapbox = new mapboxgl.Map({
+			container: 'mapbox',
+			style: `mapbox://styles/mapbox/${
+				isDarkModeEnebled ? 'dark' : 'light'
+			}-v10`,
+			zoom: 12,
+			center: [2.343756, 48.8653533],
+		});
+
+		this.mapbox.on('load', (event) => {
+			this.mapbox.resize();
+			window['map'] = this.mapbox;
+		});
+	}
+
+	switchMode(systemInitiatedDark) {
+		const mode = systemInitiatedDark.matches ? 'dark' : 'light';
+
+		document.body.setAttribute('data-theme', mode);
+		window['map'].setStyle(`mapbox://styles/mapbox/${mode}-v10`);
+	}
 }
